7ce23d0254a1e396cff9ab655bc87b0e6d353420
Nickolas <89612417+lothllann@users.noreply.github.com>
2022-09-01 15:36:55 -0300

    Cache

--- src/main/java/com/NickRuppenthal/FinalProject/resources/ProductResource.java
+++ src/main/java/com/NickRuppenthal/FinalProject/resources/ProductResource.java
@@ -7,6 +7,8 @@ import com.NickRuppenthal.FinalProject.modelo.dto.ProdutoDto;
 import com.NickRuppenthal.FinalProject.services.ProductService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,16 +25,19 @@ public class ProductResource {
     private ModelMapper mapper = new ModelMapper();
 
     @GetMapping
+    @Cacheable(value="lista_de_produtos")
     public ResponseEntity<Page<ProdutoDto>> list(){
         return ResponseEntity.ok(ProdutoDto.converter(service.list().get()));
     }
 
     @GetMapping("/{id}")
+    @Cacheable(value="lista_de_produtos")
     public ResponseEntity<ProdutoDto> findById(@PathVariable Integer id){
             return ResponseEntity.ok(new ProdutoDto(service.findById(id).get()));
     }
 
     @GetMapping("/search")
+    @Cacheable(value="lista_de_produtos")
     public ResponseEntity<Page<ProdutoDto>> search(@RequestParam(required = false) Double max_price,
                                                    @RequestParam(required = false) Double min_price,
                                                    @RequestParam(required = false) String name){
@@ -43,6 +48,7 @@ public class ProductResource {
 
     @PostMapping
     @Transactional
+    @CacheEvict(value = "lista_de_produtos", allEntries = true)
     public ResponseEntity<ProdutoDto> create(@RequestBody  ProdutoDto pDto, UriComponentsBuilder uBuilder ){
         Produto produto = service.create(pDto);
         return ResponseEntity.created(uBuilder.path("/products/{id}")
@@ -52,6 +58,7 @@ public class ProductResource {
 
     @PutMapping("/{id}")
     @Transactional
+    @CacheEvict(value = "lista_de_produtos", allEntries = true)
     public ResponseEntity<ProdutoDto> update(@PathVariable Integer id, @RequestBody  ProdutoDto pDto){
         pDto.setId(id);
         return ResponseEntity.ok().body(mapper.map(service.update(id, pDto), ProdutoDto.class));
@@ -60,6 +67,7 @@ public class ProductResource {
 
     @DeleteMapping("/{id}")
     @Transactional
+    @CacheEvict(value = "lista_de_produtos", allEntries = true)
     public ResponseEntity<DeleteDto> delete(@PathVariable Integer id){
         return ResponseEntity.ok(service.delete(id));
     }
